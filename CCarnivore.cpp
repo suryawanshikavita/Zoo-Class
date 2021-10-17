f69c237ce104cdfa2b9e22d56e828656bdeda0af
suryawanshikavita <80488071+suryawanshikavita@users.noreply.github.com>
2021-10-18 01:58:31 +0200

    optimization

--- CCarnivore.cpp
+++ CCarnivore.cpp
@@ -15,20 +15,20 @@ CCarnivore::CCarnivore(string name, int age, double weight, string sound){
 
 // This method is to get weight of an animal after hunting
 void CCarnivore::hunt() {
-	cout << "weight before hunting: " << m_dWeight << endl;
+	cout << this->getName() << "'s weight before hunting: " << m_dWeight << endl;
 	m_dWeight += 1;
-	cout << "weight after hunting: " << m_dWeight << endl;
+	cout << this->getName() << "'s weight after hunting: " << m_dWeight << endl;
 }
 
 // This method is to get weight of an animal
 // Input Animal name: Pointer of an animal name who is going to get eaten
 void CCarnivore::eat(CAnimal* animal) {
-	cout << "weight before eating other animal: " << m_dWeight << endl;
-	cout << "weight of animal to be eaten: " << animal->getWeight() << endl;
+	cout << this->getName() << "'s weight before eating " << animal->getName() << " is " << m_dWeight << endl;
+	cout << animal->getName() << "'s weight (animal to be eaten): " << animal->getWeight() << endl;
 	
 	m_dWeight += (animal->getWeight() / 2);
+	cout << this->getName() << "'s weight after eating " << animal->getName() << " is " << m_dWeight << endl;
 	animal->~CAnimal();
-	cout << "weight after eating other animal: " << m_dWeight << endl;
 }
 
 
@@ -42,17 +42,12 @@ void CCarnivore::kill() {
 void CCarnivore::status() {
 
 	int age_in_years = m_iAge / 365;
-	cout << "Age in years: " << age_in_years << endl;
-	switch (age_in_years)
-	{
-	case 1:
-		age_in_years > 3;
-		cout << "Animal is dead" << endl;
-		break;
-	case 2:
-		age_in_years <= 3;
-		cout << "Animal is alive" << endl;
-		break;
+	cout << this->getName() << "'s age in years: " << age_in_years << " years and " << m_iAge % 365 << " days" << endl;
+	if (age_in_years >= 3) {
+		cout << this->getName() << " got old and dead" << endl;
+	}
+	else {
+		cout << this->getName()  << " is young and alive" << endl;
 	}
 
 }
