55220a5dcc444a76f5894f76b112978b4e2e33a0
suryawanshikavita <80488071+suryawanshikavita@users.noreply.github.com>
2021-10-18 02:08:31 +0200

    optimization

--- CZoo.cpp
+++ CZoo.cpp
@@ -1,28 +1,71 @@
 /*
- * main.cpp
+ * CZoo.cpp
  *
  *  Created on: Oct 13, 2021
  *  Author: Kavita Suryawanshi
  */
-
 #include "CZoo.h"
-#include "CCarnivore.h"
-#include "CHerbivore.h"
-#include "CDolphin.h"		
 
-int main(int argc, char* argv[]) {
-	CCarnivore lion("Lion", 1200, 120.0, "roar");
-	CHerbivore elephant("Elephant", 300, 100.0, "trumpet");
-	CHerbivore monkey("Monkey", 1500, 80.0, "chatter");
-	CDolphin dolphin("Dolphin", 4000, 150.0, "click");
-	CCarnivore tiger("Tiger", 2000, 13.0, "roar");
+void CZoo::total_animals() {
+	int count = 0;
+	for (auto i = 0; i < Zoo.size(); i++) {
+		count += 1;
+	};
+	cout << "Total number of animals are " << count << endl;
+}
+
+//This method is to add animals
+// Input : Name of an animal to be added
+void CZoo::add_animals(CAnimal* new_animal) {
+	Zoo.push_back(new_animal);
+}
+
+// This method is to feed and to get increased weight of an animal
+void CZoo::feed() {
+
+	for (auto i = Zoo.begin(); i != Zoo.end(); i++) {
+		(*i)->feed();
+		//cout << (*i)->getName() << endl;
+	};
+}
+
+
+// This method is to get weight of an animal
+// Input animal : Pointer of an animal name, who is going to get eaten
+void CZoo::eat() {
+
+	int index_animal = rand() % Zoo.size();
+	CCarnivore* carnivore;
+	carnivore = dynamic_cast<CCarnivore*>(Zoo[index_animal]);
 	
-	CZoo MyZoo;
-	MyZoo.add_animals(&lion);
-	MyZoo.add_animals(&elephant);
-	MyZoo.add_animals(&monkey);
-	MyZoo.add_animals(&dolphin);
-	MyZoo.add_animals(&tiger);
-	
-	return 0;
+	for (auto i = Zoo.begin(); i < Zoo.end(); i++) {
+		carnivore = dynamic_cast<CCarnivore*>((*i));
+		if (carnivore != 0) {
+			cout << "Animal " << (*i)->getName() << " killed " << Zoo[index_animal]->getName() << endl;
+			(*i)->eat(Zoo[index_animal]);
+			Zoo.erase(Zoo.begin() + index_animal);
+			break;
+		}
+	}
+
+}
+
+//This method is to get the age of an animal
+// Input: int x, to increase age of an animal by x days
+void CZoo::aging(int n) {
+	CHerbivore* herbivore;
+	for (auto i = Zoo.begin(); i != Zoo.end(); i++) {
+		int increased_age = (*i)->getAge(); //increase age by x days
+		increased_age = ((*i)->getAge() +n)/ 365;
+		cout << (*i)->getName() << "'s age increased by " << n << " days and current age is " << increased_age << " years and " << (*i)->getAge() % 365 << " days" << endl;
+		
+		herbivore = dynamic_cast<CHerbivore*>(*i);
+		int displacement = 0;
+		if (herbivore != 0) { //if animal is herbivore, it will move each day
+			cout << "Object is herbivore: " << (*i)->getName() << endl;
+			displacement += n;
+			cout << "Total displacement of " << (*i)->getName() << " is : " << displacement << endl;
+		}
+		
+	};
 }
