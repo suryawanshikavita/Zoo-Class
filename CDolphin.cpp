0c43fc351e8b519cfb8c39be66fa3bde4bb0ffc2
suryawanshikavita <80488071+suryawanshikavita@users.noreply.github.com>
2021-10-18 02:00:28 +0200

    optimization

--- CDolphin.cpp
+++ CDolphin.cpp
@@ -26,18 +26,18 @@ CDolphin::CDolphin(std::string name, int age, double weight, std::string sound =
 void CDolphin::status() {
 
 	int age_in_years = m_iAge / 365;
-	std::cout << "Age in years: " << age_in_years << std::endl;
-	std::cout << "Animal is alive and never dies by age" << std::endl;
+	cout << this->getName() << "'s age in years: " << age_in_years << " years and " << m_iAge % 365 << " days" << endl;
+	cout << this->getName() << " is alive and never dies by age" << endl;
 }
 
 // This method is to get weight of an animal after hunting
 void CDolphin::hunt() {
-	std::cout << "weight after hunting: " << m_dWeight << std::endl;
+	cout << this->getName() << "'s weight does not change after hunting: " << m_dWeight << endl;
 }
 
 // This method is to feed and to get increased weight of an animal
 void CDolphin::feed() {
-	std::cout << "weight before eating: " << m_dWeight << std::endl;
+	cout << this->getName() << "'s weight before feed: " << m_dWeight << endl;
 	m_dWeight += 1.5;
-	std::cout << "weight after eating: " << m_dWeight << std::endl;
+	cout << this->getName() << "'s weight after feed: " << m_dWeight << endl;
 }
